cf3ffa59bcc020984e6edaa9255dc089d0d06744
akirk47 <andrewkirk093@gmail.com>
2018-12-28 18:41:21 -0600

    firestore

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var firebase = require("firebase");
+var firebase = require("../Config/firebase");
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp(functions.config().firebase);
+
+var db = admin.firestore();
 
 /* GET home page. */
+// router.post('/signup', function(req, res, next) {
+//   res.json({success: "this is working"});
+// });
+
 router.post('/signup', function(req, res, next) {
-  res.json({success: "this is working"});
+  var docRef = db.collection('userInfo').doc(req.body.email);
+
+  var setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
+  res.json({success: true});
 });
 
 module.exports = router;
